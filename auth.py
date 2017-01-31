f9435055c8ca95d49455464b7b7b3dd198e38517
HENRY <henryscott325@gmail.com>
2017-01-30 18:51:10 -0700

    Added some simple pass encoding, will need to relog

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 import json, os
-
+import base64
 default_auth = (None, None, None)
 
 authpath = "cfg/auth.json"
@@ -7,16 +7,18 @@ authpath = "cfg/auth.json"
 if not os.path.exists("cfg"):
     os.mkdir("cfg")
 if not os.path.exists(authpath):
-    with open(authpath, 'w+') as af:
-        af.write(json.dumps(default_auth))
+    with open(authpath, 'wb+') as af:
+        af.write(base64.b64encode(json.dumps(default_auth).encode()))
     UserAuth = default_auth
 else:
     with open(authpath, 'r') as af:
         tx = af.read()
-        UserAuth = json.loads(tx)
-
+        try:
+            UserAuth = json.loads(base64.b64decode(tx).decode())
+        except (UnicodeDecodeError, json.decoder.JSONDecodeError):
+            UserAuth = (None, None, None)
 
 def save_auth(auth):
-    with open(authpath, 'w+') as af:
-        jd = json.dumps(auth)
+    with open(authpath, 'wb+') as af:
+        jd = base64.b64encode(json.dumps(auth).encode())
         af.write(jd)
