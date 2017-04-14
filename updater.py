eafa1d0d16233010939422a86a7571ce34189e4d
HENRY <henryscott325@gmail.com>
2017-04-13 21:08:20 -0600

    Update updater

--- updater.py
+++ updater.py
@@ -55,7 +55,9 @@ def get_update(url):
     zip_ref.extractall("temp")
     zip_ref.close()
 
-    to_copy = ["python35.zip", "resources", "themes"]
+    to_copy = ["python35.zip", "resources", "themes",
+               "python36.zip", "python36.dll", "pywintypes36.dll",
+               "asyncio", "discord"]
     for cfile in to_copy:
         fn = "temp/{}".format(cfile)
         if os.path.isfile(fn):
