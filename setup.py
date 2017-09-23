1eefd7d185d65795c7e3d8e000660b58b1a9036c
henry232323 <henry@henrysmodpacks.com>
2017-09-23 09:20:42 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ build_exe_options = {
                  "asyncio.windows_utils"],
     "excludes": ["tkinter", "_tkinter", '_gtkagg', '_tkagg', 'bsddb', 'curses',
                  'pywin.debugger', 'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
-                 'unittest', 'idlelib', 'nacl', "_lzma", "_hashlib", "_bz2", "certifi"],
+                 'unittest', 'idlelib', 'nacl', "_lzma", "_hashlib", "_bz2"],
     "include_files": include_files,
     }
 
@@ -45,7 +45,7 @@ if sys.platform == "win32":
 
 setup(
     name="Pesterchum-Discord",
-    version="1.3.1",
+    version="1.3.5",
     description="A Discord client mimicking the Pesterchum chat client from Homestuck, Uses a lot of code from my Pesterchum Client.",
     options={"build_exe": build_exe_options},
     executables=[Executable("pesterchum.py", base=base, icon="resources/pc_chummy.ico"),
